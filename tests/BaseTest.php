932341e9ab836088bbb1985d466c78a0de12354f
levizoesch <lzoesch@gmail.com>
2024-05-19 22:03:51 -0700

    Update BaseTest.php

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -7,13 +7,6 @@ use ZepSDK\ZepClient;
 
 abstract class BaseTest extends \Orchestra\Testbench\TestCase
 {
-	protected $zepClient;
-
-	public function __construct($apiKey)
-	{
-		$this->zepClient = new ZepClient('z_1dWlkIjoiYmM1NzcxODAtMzE4NC00NzVkLTk5NmQtNTY5MmZhMDMzOGUwIn0.u8GZV2ZGBiYNZLE2VJ9N4mU48ZlM9SPLceATzDdbnf8axqPG_EYK2xrNYzQPtP3M8Za-AGYrjc28HlTBh65Bdg');
-	}
-
     protected function getEnvironmentSetUp($app)
     {
         // make sure, our .env file is loaded
@@ -22,14 +15,4 @@ abstract class BaseTest extends \Orchestra\Testbench\TestCase
         parent::getEnvironmentSetUp($app);
     }
 
-	/**
-	 * @throws \JsonException
-	 */
-	protected function testSessionsOrdered() {
-		// Call the sessionsOrdered method
-		$response = $this->zepClient->getSessionsOrdered();
-
-		// Assert that the response status is 200
-		$this->assertEquals(200, $response->getStatusCode());
-	}
 }
